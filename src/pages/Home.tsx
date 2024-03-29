1e8ebeafb904a28fcc873ae5641f9180dd916702
C팀 <sywon2005@gmail.com>
2024-03-29 11:14:32 +0900

    Feat: 임시 HomePage 생성

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WeekdayList from "../components/WeekdayList";
+
+const Home = () => {
+  return (
+    <div>
+      <WeekdayList />
+    </div>
+  );
+};
+
+export default Home;
