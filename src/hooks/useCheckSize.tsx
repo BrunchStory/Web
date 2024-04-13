5b52570a610022493ff2ef623c4d5475a1a873d8
C팀 <sywon2005@gmail.com>
2024-04-13 15:33:26 +0900

    Feat: 노트북일 경우 추가

--- src/hooks/useCheckSize.tsx
+++ src/hooks/useCheckSize.tsx
@@ -4,6 +4,7 @@ export const useCheckSize = () => {
   const [width, setWidth] = useState<number>(window.innerWidth);
   const [isMobile, setIsMobile] = useState<boolean>(false);
   const [isTablet, setIsTablet] = useState<boolean>(false);
+  const [isLaptop, setIsLaptop] = useState<boolean>(false);
 
   useEffect((): (() => void) => {
     const updateWidth = () => {
@@ -18,9 +19,11 @@ export const useCheckSize = () => {
   useEffect(() => {
     const checkMobile = width <= 479;
     const checkTablet = width >= 480 && width <= 768;
+    const checkLaptop = width >= 769 && width <= 1440;
     setIsMobile(checkMobile);
     setIsTablet(checkTablet);
+    setIsLaptop(checkLaptop);
   }, [width]);
 
-  return { isMobile, isTablet };
+  return { isMobile, isTablet, isLaptop };
 };
