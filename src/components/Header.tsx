084846804506c3eef37cc109690b6bf6dc511412
C팀 <sywon2005@gmail.com>
2024-04-16 11:03:46 +0900

    Design: Banner 이미지 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ import SideBar from "./Sidebar";
 import Modal from "./Modal";
 import NavHeader from "./NavHeader";
 import { Img } from "../styles/global";
+import { Link } from "react-router-dom";
 
 interface Props {
   homeProps: PropsType;
@@ -87,31 +88,49 @@ const Header = ({ homeProps }: Props) => {
 
   return (
     <HeaderContainer>
-      <BannerContainer $closed={closed.toString()} $marginTop={340}>
-        <Img
-          src="https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_1280.jpg"
-          alt="사진"
-        />
-        <CancelBtn
-          onClick={() => {
-            setClosed(true);
-          }}
-        >
-          X
-        </CancelBtn>
-        <div
-          style={{
-            position: "absolute",
-            bottom: 15,
-            left: "35%",
-            color: "black",
-            opacity: closed ? 1 : 0,
-            transition: "opacity .3s ease-in",
-            cursor: "pointer",
-          }}
-        >
-          브론치 작가 모두에게 수익의 기회가 열립니다. 응원하기 정식 오픈
-        </div>
+      <BannerContainer $closed={closed.toString()} $marginTop={420}>
+        <ImgContainer>
+          <ImgItem>
+            <Link
+              style={{
+                backgroundImage:
+                  "url(https://t1.daumcdn.net/brunch/op/a4ff48595d8e48cbba5a4086542f88b3.png)",
+              }}
+              className="custom-link"
+              to={"#"}
+            >
+              <Img
+                className="custom-img"
+                src="https://t1.daumcdn.net/brunch/op/0c8e5941f7e145bba3e16a70db29a064.png"
+                alt="사진"
+              />
+            </Link>
+            <Link
+              style={{
+                backgroundImage:
+                  "url(https://t1.daumcdn.net/brunch/op/010273fe05c840298af803622ab5d5f3.png)",
+                opacity: closed ? 1 : 0,
+                transition: "opacity 0.5s ease-in-out",
+              }}
+              className="custom-link-2"
+              to={"#"}
+            >
+              <Img
+                className="custom-img-2"
+                src="https://t1.daumcdn.net/brunch/op/31f5292636424bd399a728ff7a3d47d6.png"
+                alt="사진"
+              />
+            </Link>
+          </ImgItem>
+
+          <CancelBtn
+            onClick={() => {
+              setClosed(true);
+            }}
+          >
+            X
+          </CancelBtn>
+        </ImgContainer>
       </BannerContainer>
       <NavHeader headerprops={headerProps} />
 
@@ -139,18 +158,74 @@ const BannerContainer = styled.div<{
   $closed: string;
   $marginTop: number;
 }>`
-  width: 100%;
-  height: 400px;
-  overflow: hidden;
   position: relative;
+  overflow: hidden;
+  width: 100%;
+  height: 480px;
+  background-size: cover;
   margin-top: ${(props) =>
     props.$closed === "true" ? `-${props.$marginTop}px` : "0px"};
   transition: margin 0.5s ease-in-out;
 `;
 
+const ImgContainer = styled.ul`
+  overflow: hidden;
+  width: 100%;
+  height: 480px;
+`;
+
+const ImgItem = styled.li`
+  position: absolute;
+  z-index: 2;
+  top: 0;
+  left: 0;
+  display: block;
+  overflow: hidden;
+  width: 100%;
+  transform: none;
+  transition: transform 0.5s ease-in-out;
+
+  .custom-link {
+    height: 480px;
+    background-size: cover;
+    display: block;
+    width: 100%;
+    background-position: 50% 50%;
+    background-repeat: no-repeat;
+    text-align: center;
+  }
+
+  .custom-img {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 960px;
+    margin: -240px 0 0 -480px;
+  }
+
+  .custom-link-2 {
+    position: absolute;
+    bottom: 0;
+    height: 60px;
+    display: block;
+    width: 100%;
+    background-position: 50% 50%;
+    background-repeat: no-repeat;
+    text-align: center;
+  }
+
+  .custom-img-2 {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 960px;
+    margin: -30px 0 0 -480px;
+  }
+`;
+
 const CancelBtn = styled.button`
   position: absolute;
-  top: 50%;
+  top: 45%;
   right: 10%;
   cursor: pointer;
   background-color: transparent;
@@ -158,4 +233,5 @@ const CancelBtn = styled.button`
   border: none;
   font-size: 3.65rem;
   font-weight: 10;
+  z-index: 3;
 `;
