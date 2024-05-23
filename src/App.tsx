c0867bbc73a33d4355fb12aa898e984439740c64
C팀 <sywon2005@gmail.com>
2024-05-24 01:48:11 +0900

    Feat: apply 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import "./styles/global.css";
+import Apply from "./pages/Apply";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />}>
           <Route path=":day/:sorted" element={<Home />} />
         </Route>
+        <Route path="/apply" element={<Apply />} />
       </Routes>
     </BrowserRouter>
   );
