babf7c8e47543d93bf4a9a1b549cd55d50605302
C팀 <sywon2005@gmail.com>
2024-05-28 11:45:03 +0900

    Refactor: reducer 수정

--- src/store/reducer/reducer.tsx
+++ src/store/reducer/reducer.tsx
@@ -1,12 +1,11 @@
-import { combineReducers } from "@reduxjs/toolkit";
 import loginSlice from "../slices/loginSlice";
 import sidebarSlice from "../slices/sidebarSlice";
 import navheaderSlice from "../slices/navheaderSlice";
 
-const reducer = combineReducers({
+const reducer = {
   login: loginSlice,
   sidebar: sidebarSlice,
   navheader: navheaderSlice,
-});
+};
 
 export default reducer;
